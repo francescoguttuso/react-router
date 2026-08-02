8262b4ccad5ab8013fb4a0cdfa5288526ba2b517
francesco-guttuso <francescoguttuso@gmail.com>
2026-08-03 01:39:53 +0200

    feat: refactor App component to use ShellLayout and enhance Product page with Axios data fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import { Routes, Route } from "react-router";
+import { ShellLayout } from "./components/ShellLayout";
 import { Home } from "./pages/Home";
 import { Product } from "./pages/Product";
 import { About } from "./pages/About";
@@ -8,9 +9,11 @@ import { About } from "./pages/About";
 export const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/product" element={<Product />} />
+      <Route element={<ShellLayout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="product" element={<Product />} />
+      </Route>
     </Routes>
   );
 };
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,9 +1,39 @@
-import { NavLink } from "react-router";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 export const Product = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    axios
+      .get("https://fakestoreapi.com/products")
+      .then((response) => {
+        setProducts(response.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error("Errore nella richiesta:", err);
+        setError("Impossibile caricare i prodotti. Riprova più tardi.");
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <h2>Caricamento prodotti con Axios...</h2>;
+  if (error) return <h2 style={{ color: "red" }}>{error}</h2>;
+
   return (
-    <>
-      <h1>I nostri Prodotti</h1>
-      <NavLink to="/">Torna alla Homepage</NavLink>
-    </>
+    <div>
+      <h1>I nostri Prodotti </h1>
+      <div className="products-grid">
+        {products.map((item) => (
+          <article key={item.id} className="product-card">
+            <img src={item.image} alt={item.title} width="100" />
+            <h3>{item.title}</h3>
+            <p>{item.price} €</p>
+          </article>
+        ))}
+      </div>
+    </div>
   );
 };
