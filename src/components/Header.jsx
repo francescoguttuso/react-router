3bc45e6bc421089ef30657586192058b0544e0b1
francesco-guttuso <francescoguttuso@gmail.com>
2026-08-03 01:29:28 +0200

    feat: implement Header component with navigation links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,29 @@
+import { NavLink } from "react-router";
+
 export const Header = () => {
-  <></>;
+  return (
+    <header className="header">
+      <nav className="navbar">
+        <NavLink
+          to="/"
+          end
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/about"
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          Chi siamo
+        </NavLink>
+        <NavLink
+          to="/product"
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          Prodotti
+        </NavLink>
+      </nav>
+    </header>
+  );
 };
