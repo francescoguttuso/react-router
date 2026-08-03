c24761f3394dee5047b7f698dc093ac177d35c33
francescoguttuso <francescoguttuso@gmail.com>
2026-08-03 14:49:32 +0200

    feat: implement NotFound component with 404 error message and navigation link

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,17 @@
+import { Link } from "react-router";
 import "./NotFound.css";
 
 export const NotFound = () => {
-  return <></>;
+  return (
+    <div className="not-found-container">
+      <h1 className="not-found__code">404</h1>
+      <h2 className="not-found__title">Ops! Pagina non trovata</h2>
+      <p className="not-found__text">
+        La pagina che stai cercando non esiste o è stata spostata.
+      </p>
+      <Link to="/" className="not-found__link">
+        Torna alla Home
+      </Link>
+    </div>
+  );
 };
